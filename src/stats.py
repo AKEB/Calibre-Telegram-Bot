53c9c8b6d1cfeff38690e8900388e0fb9fa9d284
Vadim Babadzhanyan <vadim.babadzhanyan@my.games>
2025-06-25 18:39:02 +0300

    calibre-telegram-bot

--- src/stats.py
+++ src/stats.py
@@ -38,7 +38,7 @@ def get_stats() -> dict:
         if tags_count:
             stat['categories'] = int(tags_count[0])
 
-        cursor.execute("SELECT lang_code,COUNT(*) FROM languages GROUP BY lang_code")
+        cursor.execute("SELECT L.lang_code, COUNT(B.*) FROM books_languages_link as B JOIN languages as L ON B.lang_code=L.id GROUP BY B.lang_code")
         languages_count = cursor.fetchall()
         if languages_count:
             stat['languages'] = {lang[0]: int(lang[1]) for lang in languages_count}
